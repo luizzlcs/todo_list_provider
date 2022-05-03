098e3356b71f214c47a90bf792f93e1c29f6859c
Luiz Carlos <luizzlcs@hotmail.com>
2022-05-03 10:15:53 -0300

    Aplicando routers e Provider

--- lib/services/app_widget.dart
+++ lib/services/app_widget.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:todo_list_provider/modules/splash/splash_page.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_list_provider/models/auth/login/login_controller.dart';
+import 'package:todo_list_provider/models/auth/login/login_page.dart';
+import '../models/splash/splash_page.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      title:  'Todo Lis Provider',
-      home:  SplashPage(),
+    return MaterialApp(
+      title: 'Todo Lis Provider',
+      routes: {
+        '/login': (context) => MultiProvider(
+              providers: [
+                Provider(
+                  create: (_) => 'Repository',
+                ),
+                Provider(
+                  create: (_) => 'Service',
+                ),
+                ChangeNotifierProvider(create: (_) => LoginController()),
+              ],
+              child: LoginPage(),
+            ),
+        '/register': (context) => MultiProvider(
+              providers: [
+                Provider(
+                  create: (_) => 'Repository',
+                ),
+                Provider(
+                  create: (_) => 'Service',
+                ),
+                ChangeNotifierProvider(create: (_) => LoginController()),
+              ],
+              child: LoginPage(),
+            ),
+      },
+      home: SplashPage(),
     );
   }
 }
